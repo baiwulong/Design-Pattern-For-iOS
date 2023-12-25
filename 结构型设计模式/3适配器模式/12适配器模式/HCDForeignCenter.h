90f22f0d9886417bd138cdaf62ea460bb89038a5
baiwulong <1204803180@qq.com>
2023-12-25 18:36:32 +0800

    改变结构

---
+++ 结构型设计模式/3适配器模式/12适配器模式/HCDForeignCenter.h
@@ -0,0 +1,18 @@
+//
+//  HCDForeignCenter.h
+//  12适配器模式
+//
+//  Created by 黄成都 on 15/8/14.
+//  Copyright (c) 2015年 黄成都. All rights reserved.
+//
+
+// 旧接口
+
+#import <Foundation/Foundation.h>
+
+@interface HCDForeignCenter : NSObject
+@property(nonatomic,copy)NSString *name;
+-(void)foreignAttact;
+-(void)foreignDefent;
+-(instancetype)initWithName:(NSString *)name;
+@end
